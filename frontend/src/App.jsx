9ec5237248699d13be83608b4f9a40368562e28b
Laiba121605 <laibayasir16dec@gmail.com>
2026-05-19 17:00:23 +0500

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,22 +5,25 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
+import LogActivity from './pages/LogActivity';
+import Chat from './pages/Chat';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* Navbar can be placed here if it needs to be globally visible */}
       <Navbar />
       <Routes>
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        
+
         {/* Protected Routes */}
         <Route element={<ProtectedRoute />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/dashboard"    element={<Dashboard />}    />
+          <Route path="/profile"      element={<Profile />}      />
+          <Route path="/log-activity" element={<LogActivity />}  />
+          <Route path="/chat"         element={<Chat />}         />
         </Route>
       </Routes>
     </BrowserRouter>
