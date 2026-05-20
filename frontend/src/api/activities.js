e4403cc323b704ed95d74874e0f5a80f6a0a9c8e
Laiba121605 <laibayasir16dec@gmail.com>
2026-05-20 22:16:26 +0500

    Add files via upload

--- frontend/src/api/activities.js
+++ frontend/src/api/activities.js
@@ -1,10 +1,19 @@
 import api from "./axios";
 
-export const logSleep        = (data) => api.post("/activities/sleep",        data).then(r => r.data);
-export const logTemperature  = (data) => api.post("/activities/temperature",  data).then(r => r.data);
-export const logDiaper       = (data) => api.post("/activities/diaper",       data).then(r => r.data);
-export const logFeeding      = (data) => api.post("/activities/feeding",      data).then(r => r.data);
-export const logCrying       = (data) => api.post("/activities/crying",       data).then(r => r.data);
+export const logSleep = (childId, data) => 
+  api.post("/activities/sleep", { child_id: childId, ...data }).then(r => r.data);
 
-export const getActivities   = (childId, date) =>
+export const logTemperature = (childId, data) => 
+  api.post("/activities/temperature", { child_id: childId, ...data }).then(r => r.data);
+
+export const logDiaper = (childId, data) => 
+  api.post("/activities/diaper", { child_id: childId, ...data }).then(r => r.data);
+
+export const logFeeding = (childId, data) => 
+  api.post("/activities/feeding", { child_id: childId, ...data }).then(r => r.data);
+
+export const logCrying = (childId, data) => 
+  api.post("/activities/crying", { child_id: childId, ...data }).then(r => r.data);
+
+export const getActivities = (childId, date) =>
   api.get(`/activities/${childId}`, { params: { date } }).then(r => r.data);
